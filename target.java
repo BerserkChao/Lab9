4f757f8399155d8f2e6f0947e3d895290eb5ee4a
%username% <%email%>
2024-05-15 11:48:54 -0400

    Targets now get destroyed when hit by a cannon ball

--- target.java
+++ target.java
@@ -1,11 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
-/**
- * Write a description of class target here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class target extends Actor
 {
     /**
@@ -14,6 +10,22 @@ public class target extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        List<CannonBall> balls = getWorld().getObjects(CannonBall.class);
+        
+        for (int i=0; i < balls.size(); i++){
+            CannonBall ball = balls.get(i);
+            
+            Vector2D targetToBall = new Vector2D(ball.getX() - getX(), ball.getY() - getY());
+            double distance = targetToBall.magnitude();
+            
+            double ballRadius = ball.getImage().getHeight() / 2;
+            double targetRadius = getImage().getHeight() / 2;
+            
+            if (distance < ballRadius + targetRadius){
+                setImage("targetDestroyed.png");
+                Greenfoot.playSound("explosion.wav");
+            }
+        }
+        
     }
 }
